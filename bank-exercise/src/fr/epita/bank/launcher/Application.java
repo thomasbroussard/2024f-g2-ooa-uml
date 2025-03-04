b3a50fb8b5dc6e87907ca18e21a91e3c4d47e8f8
Présentation <tbr@tbr.com>
2025-03-04 10:30:41 +0100

    implement application flow

--- bank-exercise/src/fr/epita/bank/launcher/Application.java
+++ bank-exercise/src/fr/epita/bank/launcher/Application.java
@@ -12,25 +12,32 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("Welcome to this bank application");
-        System.out.println("What would you like to do?");
-        System.out.println("1. create a customer");
-        System.out.println("2. withdraw money");
-        System.out.println("9. quit the application");
-        Scanner scanner = new Scanner(System.in);
-        String answer = scanner.nextLine();
-        List<SavingsAccount> accounts = new ArrayList<>();
+        //
+        String answer = "";
+        do {
+            System.out.println("What would you like to do?");
+            System.out.println("1. create a customer");
+            System.out.println("2. withdraw money");
+            System.out.println("3. print list of customers");
+            System.out.println("9. quit the application");
+            Scanner scanner = new Scanner(System.in);
+            answer = scanner.nextLine();
+            List<SavingsAccount> accounts = new ArrayList<>();
+            if (answer.equals("1")) {
+                Customer customer = createCustomerFromUserInput(scanner);
+                SavingsAccount savingsFromUserInput = createSavingsFromUserInput(scanner, customer);
+                accounts.add(savingsFromUserInput);
+                //TODO add customer in a customers list
+            } else if (answer.equals("2")) {
+                //account selection before, take the first account for now
+                withdrawMoney(scanner, accounts.get(0));
+            } else if (answer.equals("3")){
+                //TODO implement
+            } else {
 
-        if (answer.equals("1")){
-            Customer customer = createCustomerFromUserInput(scanner);
-            SavingsAccount savingsFromUserInput = createSavingsFromUserInput(scanner, customer);
-            accounts.add(savingsFromUserInput);
-        } else if (answer.equals("2")){
-            //account selection before, take the first account for now
-
-            withdrawMoney(scanner, accounts.get(0));
-        } else {
-            System.out.println("option not recognized, try again");
-        }
+                System.out.println("option not recognized, try again");
+            }
+        }while (!"9".equals(answer));
 
     }
 }
