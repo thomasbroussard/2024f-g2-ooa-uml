00712db948205be31a0860fe8acd34c0873840a7
Présentation <tbr@tbr.com>
2025-03-10 11:24:12 +0100

    data mapping

--- biostats/test/fr/epita/biostats/tests/SandboxTest.java
+++ biostats/test/fr/epita/biostats/tests/SandboxTest.java
@@ -1,5 +1,7 @@
 package fr.epita.biostats.tests;
 
+import fr.epita.biostats.datamodel.BiostatEntry;
+
 public class SandboxTest {
 
     public static void main(String[] args) {
@@ -7,15 +9,30 @@ public class SandboxTest {
 
         String[] parts = line.split(",");
 
-        String cleanName = parts[0].replace("\"", "");
+        String cleanName = parts[0].replace("\"", "").trim();
+        String cleanGender = parts[1].replace("\"", "").trim();
+        String cleanAgeAsString = parts[2].replace("\"", "").trim();
+        Integer cleanAge = Integer.parseInt(cleanAgeAsString);
+        String cleanHeightAsString = parts[3].replace("\"", "").trim();
+        Integer cleanHeight = Integer.parseInt(cleanAgeAsString);
+        String cleanWeightAsString = parts[4].replace("\"", "").trim();
+        Integer cleanWeight = Integer.parseInt(cleanAgeAsString);
 
         System.out.println(cleanName);
 
-        String cleanHeight = parts[3].trim();
 
         System.out.println(cleanHeight);
-        System.out.println(Integer.parseInt(cleanHeight));
 
         //map the array "parts" to a BiostatEntry instance
+
+        BiostatEntry entry = new BiostatEntry(
+                cleanName,
+                cleanGender,
+                cleanAge,
+                cleanHeight,
+                cleanWeight
+        );
+
+
     }
 }
