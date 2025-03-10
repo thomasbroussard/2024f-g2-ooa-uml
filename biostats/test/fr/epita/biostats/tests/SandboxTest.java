c8da777e68e4fbc80f802fcd82c0a848e1cce553
Présentation <tbr@tbr.com>
2025-03-10 10:39:46 +0100

    new csv file

---
+++ biostats/test/fr/epita/biostats/tests/SandboxTest.java
@@ -0,0 +1,19 @@
+package fr.epita.biostats.tests;
+
+public class SandboxTest {
+
+    public static void main(String[] args) {
+        String line =  "\"Alex\",       \"M\",   41,       74,      170";
+
+        String[] parts = line.split(",");
+
+        String cleanName = parts[0].replace("\"", "");
+
+        System.out.println(cleanName);
+
+        String cleanHeight = parts[3].trim();
+
+        System.out.println(cleanHeight);
+        System.out.println(Integer.parseInt(cleanHeight));
+    }
+}
